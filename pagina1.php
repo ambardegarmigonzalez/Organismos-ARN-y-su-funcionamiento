1069d5abdba398a1b76158fd6a711402ead5e1ce
ambardegarmigonzalez <ambardegarmigonzalez@cobaqroo.edu.mx>
2026-03-25 12:45:14 -0500

    Add files via upload

---
+++ pagina1.php
@@ -0,0 +1,145 @@
+<?php
+?>
+<!Doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title> Organismos: ARN y su funcionamiento</title>
+    <link rel="stylesheet" href="css/Style2.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <!--
+        { <ul> } es una lista sin orden para crear la lista que será el menú
+
+        { <li> } es el elemento de la lista en el que cada subtema es un punto de la lista
+
+        { <a href= "#quees"> } es un enlace para dirigirse al { id="arn" }, lo que hay que cambiar para 
+        dirigirse a otro lado es el nombre del id dentro del enlace y en el código
+
+        { style= "..."} sirve para modificar el css en la etiqueta que se esta poniendo
+
+        { list-style: none; } es para quitar los puntos de la lista
+
+        { display: flex; } es para poner los elementos de la lista en una sola línea (horizontal)
+
+        { gap: 20px; } es para poner un espacio entre cada elemento de la lista, se puede cambiar con el número
+
+        { justify-content: flex-end; } es para alinear el menú a la derecha, la alineacion se puede cambiar 
+         al cambiar la ultima palabra por { center } para el centro o { flex-start } para alinear a la izquierda
+
+        { nav } sirve para que el código sepa que es un área de navegación, es decir, un menú
+
+        { padding-right: 30px; } es para poner un espacio entre el menú y el borde derecho, se puede cambiar 
+        con el número
+        -->
+        <div class="imagen"></div>    
+        <br>
+        <div class="component">
+            <nav>
+                <ul style="list-style: none; display: flex; gap: 20px; justify-content: flex-start; padding-left: 30px;">
+                    <li><a href="#arn">ARN</a></li>
+                    <li><a href="#estructura">Niveles de Estructura</a></li>
+                </ul>
+            </nav>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="titulin">
+            <h1 style="text-align: left;">Organismos: ARN y su funcionamiento</h1> <br>
+            <h5>Esta página hablaré sobre la composicion y niveles de las moléculas del ARN, y el proceso<br> 
+                que conlleva el cuerpo Humano, con el fin de enter el comportamiento de cada ser vivo.
+            </h5>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+
+    <section>
+
+        <section id="arn">
+            <div class="row" style="padding: 20px;">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="Imagenes/ARN.jfif" class="card-img-top" alt="Imagen 1">
+                        <div class="card-body">
+                            <h3 class="card-title" style="text-align: center;">ARN</h3>
+                            <p class="card-text" style="text-align: justify;">Es una molécula esencial presente en todas las células vivas que actúa como intermediario para transmitir las instrucciones del ADN y sintetizar proteínas</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="Imagenes/nucleo.jpg" class="card-img-top" alt="Imagen 2">
+                        <div class="card-body">
+                            <h3 class="card-title" style="text-align: center;">¿Donde se Ubica?</h3>
+                            <p class="card-text" style="text-align: justify;">En ek nucleo o citoplasma de la celula</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="Imagenes/azucar.png" class="card-img-top" alt="Imagen 3">
+                        <div class="card-body">
+                            <h3 class="card-title" style="text-align: center;">¿Qué azucar tiene?</h3>
+                            <p class="card-text" style="text-align: justify;">Rbibosomas</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="estructura">
+            <div class="row" style="padding: 20px;">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="Imagenes/nivel1.png" class="card-img-top" alt="Imagen 1">
+                        <div class="card-body">
+                            <h3 class="card-title" style="text-align: center;">Nivel 1</h3>
+                            <p class="card-text" style="text-align: justify;">Secuencia lineal de nucleótidos (bases nitrogenadas).</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="Imagenes/nivel2.webp" class="card-img-top" alt="Imagen 2">
+                        <div class="card-body">
+                            <h3 class="card-title" style="text-align: center;">Nivel 2</h3>
+                            <p class="card-text" style="text-align: justify;">La cadena simple se pliega sobre sí misma mediante apareamiento de bases (A-U, G-C), formando horquillas, bucles y hélices.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="Imagenes/nivel3.png" class="card-img-top" alt="Imagen 3">
+                        <div class="card-body">
+                            <h3 class="card-title" style="text-align: center;">Nivel 3</h3>
+                            <p class="card-text" style="text-align: justify;">Plegamiento tridimensional complejo de la estructura secundaria, crucial para la función de la molécula</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="cita-bibliografica">
+                <span class="autor">Méndez Rosales, M.E.</span>
+                <span class="ano">(2025)</span>
+                <span class="titulo">Organismos: estructuras y procesos. Herencia y evolución</span>
+                <span class="editorial">Book Mart, S.A de C.V.</span>
+            </div>
+
+        </section>
+    </section>
+
+</body>
+
+</html>
